2112852d58cf932eefed8fdcea66e4ebed32c36d
apobekiaris <apostolis.bekiaris@gmail.com>
2024-10-29 13:28:32 +0200

    fix designer errors

--- CS/OutlookInspired.Win/Editors/ColumnViewUserControl.cs
+++ CS/OutlookInspired.Win/Editors/ColumnViewUserControl.cs
@@ -7,10 +7,11 @@ namespace OutlookInspired.Win.Editors{
 
     
     public partial class ColumnViewUserControl : UserControl, IColumnViewUserControl{
-        public ColumnView ColumnView=>(ColumnView)Controls.OfType<GridControl>().First().MainView;
+        public ColumnView ColumnView=>(ColumnView)Controls.OfType<GridControl>().FirstOrDefault()?.MainView;
         public ColumnViewUserControl() => Load += OnLoad;
 
-        private void OnLoad(object sender, EventArgs e){
+        private void OnLoad(object sender, EventArgs e) {
+            if (ColumnView == null) return;
             ColumnView.ColumnFilterChanged += (_, _) => OnDataSourceOrFilterChanged();
             ColumnView.DataSourceChanged += (_, _) => OnDataSourceOrFilterChanged();
             ColumnView.DataError+=(_, args) => throw new AggregateException(args.DataException.Message,args.DataException);
